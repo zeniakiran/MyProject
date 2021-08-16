7cb01b15e389308cd23a3c636a45b1a68f7e2190
zeniakiran <67507784+zeniakiran@users.noreply.github.com>
2021-08-16 15:47:18 +0500

    commit from vscode

--- myApp/app.js
+++ myApp/app.js
@@ -9,7 +9,6 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -22,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+//    catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
