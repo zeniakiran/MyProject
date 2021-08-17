7a0722cadef21f41dd47b4643fea3ab5b13b0f97
zeniakiran <67507784+zeniakiran@users.noreply.github.com>
2021-08-17 09:18:22 +0500

    Trying commit..

--- myApp/app.js
+++ myApp/app.js
@@ -22,6 +22,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 //   catch 404 and forward to error handler
+//   error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
